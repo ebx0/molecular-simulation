3ca868bc13a82b152c76403bd16fdda74d6364fe
Eren Bulbul <erenbulbul@protonmail.com>
2022-02-04 00:17:32 +0300

    Will rewrite.

--- bonding-simulator.py
+++ bonding-simulator.py
@@ -1,12 +1,14 @@
 from mpmath import sech
+from PIL import Image
 from monoatomicsim import monoasim
 import numpy as np
 
 distulist = []
 images = []
 
+#Will be rewrite. Currently unavalible
 for j in range(2):
-    plotsize = 1024
+    plotsize = 512
     center = int(plotsize/2)
     size = 0.8
     
@@ -17,23 +19,33 @@ for j in range(2):
         ii = int(i*2) # Only calculate half of possible area, for performance improvement
         x = (ii-center)/center/(size/3)
         if j == 0: 
-            functionvalue = int(distuscale*(0.4*sech(2*x**2) - 0.4*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation
+            functionvalue = int(distuscale*(0.4*sech(3.25*x**2) - 0.45*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation
         if j == 1:
-            functionvalue = int(distuscale*(sech(2**5*x**3))) # Specific function of particle disturbation
+            functionvalue = int(distuscale*(sech(2**5*x**2))) # Specific function of particle disturbation
         distu[ii:ii+1] = [functionvalue,functionvalue]
     distulist.append(distu)
     
 steps = 48
 distusteps = []
-for i in range(48):
+for i in range(steps):
     distusteps.append(distulist[0])
     
 for step in range(steps):
     distusteps.append(list(np.add( np.multiply(distulist[1],(step/steps)) , np.multiply(distulist[0],(1-(step/steps))) ).astype("int")))
 
-for i in range(48):
+for i in range(steps):
     distusteps.append(distulist[-1])
 
+graph = []
 for i in range(len(distusteps)):
-    (monoasim(plotsize, center, size, distusteps[i])).save(f"images/{i+1}.jpg")
-    print(f"Processing ({i+1}/{len(distusteps)})")
+    graph.append(monoasim(plotsize, center, size, distusteps[i]))
+    print(f"Processing (1/2) ({i+1}/{len(distusteps)})")
+    
+
+for i in range(len(graph)):
+    Image.fromarray(graph[i]).save(f"images/{i+1}.jpg")
+    print(f"Processing (2/2) ({i+1}/{len(distusteps)})")
+    
+for i in range(len(graph)):
+    Image.fromarray(graph[i]).save(f"images/{i+1}.jpg")
+    print(f"Processing (2/2) ({i+1}/{len(distusteps)})")
