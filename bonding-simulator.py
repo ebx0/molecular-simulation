631d2665ea8b62aa06dd940ceadca53df05c59d8
Eren Bulbul <erenbulbul@protonmail.com>
2022-02-04 01:17:23 +0300

    Bonding fixed and heatmap added to in

--- bonding-simulator.py
+++ bonding-simulator.py
@@ -6,46 +6,61 @@ import numpy as np
 distulist = []
 images = []
 
+initial = 2
+final = 1
+
+scale = 8 # resolution of heatmap of electrons
+
 #Will be rewrite. Currently unavalible
 for j in range(2):
-    plotsize = 512
+    plotsize = 1024
     center = int(plotsize/2)
     size = 0.8
-    
     distuscale = 5*10**2
     distu = [0] * plotsize
     
     for i in range(int(plotsize/2)):
         ii = int(i*2) # Only calculate half of possible area, for performance improvement
         x = (ii-center)/center/(size/3)
-        if j == 0: 
-            functionvalue = int(distuscale*(0.4*sech(3.25*x**2) - 0.45*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation
-        if j == 1:
-            functionvalue = int(distuscale*(sech(2**5*x**2))) # Specific function of particle disturbation
+        if initial == 2 and final == 1: #If lose an electron
+            if j == 0: 
+                functionvalue = int(distuscale*(0.4*sech(3.25*x**2) - 0.45*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation
+            if j == 1:
+                functionvalue = int(distuscale*(sech(2**5*x**2))) # Specific function of particle disturbation
+        elif initial == 1 and final == 2: # If gain an electron
+            if j == 0: 
+                functionvalue = int(distuscale*(sech(2**5*x**2))) # Specific function of particle disturbation
+            if j == 1:
+                functionvalue = int(distuscale*(0.4*sech(3.25*x**2) - 0.45*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation        
         distu[ii:ii+1] = [functionvalue,functionvalue]
     distulist.append(distu)
     
-steps = 48
+steps = 120
 distusteps = []
-for i in range(steps):
-    distusteps.append(distulist[0])
-    
-for step in range(steps):
-    distusteps.append(list(np.add( np.multiply(distulist[1],(step/steps)) , np.multiply(distulist[0],(1-(step/steps))) ).astype("int")))
 
-for i in range(steps):
-    distusteps.append(distulist[-1])
+for step in range(steps):
+    initialshare = np.multiply(distulist[1],(step/(steps-1))) # 100% when first step, 0% when last step
+    finalshare = np.multiply(distulist[0],(1-(step/(steps-1))))
+    distusteps.append(list(np.add(initialshare, finalshare).astype("int"))) # Find the disturbation line for every stage of motion
 
 graph = []
-for i in range(len(distusteps)):
-    graph.append(monoasim(plotsize, center, size, distusteps[i]))
+for i in range(steps):
+    graph.append(monoasim(plotsize, center, size, distusteps[i], scale))
     print(f"Processing (1/2) ({i+1}/{len(distusteps)})")
     
+for i in range(len(graph)):
+    image = (graph[i])[0]
+    image2 = (graph[i])[1] # Heatmap
 
-for i in range(len(graph)):
-    Image.fromarray(graph[i]).save(f"images/{i+1}.jpg")
-    print(f"Processing (2/2) ({i+1}/{len(distusteps)})")
+    image = np.multiply(image,128) # color with 128
     
-for i in range(len(graph)):
-    Image.fromarray(graph[i]).save(f"images/{i+1}.jpg")
+    imagetemp = image2 # to upscale the heatmap
+    image2 = np.zeros((plotsize, plotsize, 1), dtype=np.uint8) # upscaled blank plot to paint
+    for x in range(int(plotsize/scale)):
+        for y in range(int(plotsize/scale)):
+            image2[x*scale:(x+1)*scale, y*scale:(y+1)*scale] = imagetemp[x,y]
+            
+    concat = np.concatenate((image,image2), axis=1)
+            
+    Image.fromarray(np.concatenate((concat,concat,concat), axis=2)).save(f"images/{i+1}.jpg")
     print(f"Processing (2/2) ({i+1}/{len(distusteps)})")
--- monoatomicsim.py
+++ monoatomicsim.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from PIL import Image
 from mpmath import sech
 from math import dist
@@ -11,18 +11,18 @@ size = 0.8
 distuscale = 5*10**2
 distu = [0] * plotsize
 scale = 8 #scale of heatmap of particles
+
     
 for i in range(int(plotsize/2)):
    ii = int(i*2) # Only calculate half of possible area, for performance improvement
    x = (ii-center)/center/(size/3)
-   functionvalue = int(distuscale*(0.4*sech(3.28
-                                            *x**2) - 0.45*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation
+   functionvalue = int(distuscale*(0.4*sech(3.28*x**2) - 0.45*sech(5*x**2) + sech(2**5*x**2))) # Specific function of particle disturbation
    distu[ii:ii+1] = [functionvalue,functionvalue]
 
 def monoasim(plotsize, center, size, distu, scale):
-    data = numpy.zeros((plotsize, plotsize, 1), dtype=numpy.uint8)
-    #data2 = numpy.zeros((plotsize, plotsize, 1), dtype=numpy.uint8)
-    #data3 = numpy.zeros((plotsize, plotsize, 1), dtype=numpy.uint8)
+    data = np.zeros((plotsize, plotsize, 1), dtype=np.uint8)
+    #data2 = np.zeros((plotsize, plotsize, 1), dtype=np.uint8)
+    #data3 = np.zeros((plotsize, plotsize, 1), dtype=np.uint8)
     
     distuscale = 5*10**2
     #for i in range(int(len(distu)/2)):
@@ -49,33 +49,33 @@ def monoasim(plotsize, center, size, distu, scale):
                 #realdistu[x] += realdistuconstant # Save the data for analysis
     
     # Draw the real disturbation of particles
-    #data2 = numpy.zeros((plotsize, plotsize, 1), dtype=numpy.uint8)
+    #data2 = np.zeros((plotsize, plotsize, 1), dtype=np.uint8)
     #for i in range(len(realdistu)):
     #    data2[i,-(realdistu[i]+1):-1] = 1
     
     
     # getting an heatmap for particles
     miniplotsize = int(plotsize/scale)
-    average = numpy.zeros((miniplotsize, miniplotsize, 1), dtype=numpy.uint8)
+    average = np.zeros((miniplotsize, miniplotsize, 1), dtype=np.uint8)
     for x in range(miniplotsize):
         for y in range(miniplotsize):
-            average[x,y] = numpy.sum(data[x*scale:(x+1)*scale,y*scale:(y+1)*scale])
-    print(numpy.sum(average))
-    average = numpy.multiply(average, 255/(scale**2)).astype('uint8')
+            average[x,y] = np.sum(data[x*scale:(x+1)*scale,y*scale:(y+1)*scale])
+    average = np.multiply(average, 255/(scale**2)).astype('uint8')
     
     return(data, average)
 
-image = numpy.multiply((monoasim(plotsize, center, size, distu, scale))[0],128)
+# Make signals to color
+image = np.multiply((monoasim(plotsize, center, size, distu, scale))[0],128)
 image2 = (monoasim(plotsize, center, size, distu, scale))[1]
 
 #enlarge the image2 (average map) to normal plot size
 imagetemp = image2
-image2 = numpy.zeros((plotsize, plotsize, 1), dtype=numpy.uint8)
+image2 = np.zeros((plotsize, plotsize, 1), dtype=np.uint8)
 for x in range(int(plotsize/scale)):
     for y in range(int(plotsize/scale)):
         image2[x*scale:(x+1)*scale,y*scale:(y+1)*scale] = imagetemp[x,y]
     
 
-Image.fromarray(numpy.concatenate((image,image,image), axis=2)).show()
-Image.fromarray(numpy.concatenate((image2,image2,image2), axis=2)).show()
+#Image.fromarray(np.concatenate((image,image,image), axis=2)).show()
+#Image.fromarray(np.concatenate((image2,image2,image2), axis=2)).show()
 
