e7c40baab7e93c0ee9da5718fc25f4d91d00266e
Eren Bulbul <erenbulbul@protonmail.com>
2022-02-08 16:58:30 +0300

    molecular simulation optimized.

--- molecularsimulator.py
+++ molecularsimulator.py
@@ -9,32 +9,36 @@ from atom import atom
 
 
 phase = 2
-area = 512*4
+area = 512*8
 plotsize = 512 # size of requested plot
 scaledsize = int(plotsize / 8)
 
 atoms = []
 
-for i in range(10):
-    atoms.append(atom([randint(100, area-100),randint(100, area-100)],[(random()-0.5)*5,(random()-0.5)*5],phase))
-    
-#atoms.append(atom([1,1],[20,20],phase))
-#atoms.append(atom([700,700],[-20,-20],phase))
+for i in range(64):
+    atoms.append(atom([randint(scaledsize, area-plotsize),randint(scaledsize, area-plotsize)],[(random()-0.5)*15,(random()-0.5)*15],phase))
 
-steps = 240
+steps = 480
+
+datas = []
+for i in range(50):
+    distu = distugraph(2, plotsize)
+    datas.append(monoasim(plotsize, distu))
+    print(f"part 1 - {i}/50")
 
 for i in range(steps):
     
     bg = [0,0,[0,0]]
-    bg[0] = np.zeros((plotsize*8, plotsize*8, 3), dtype=np.uint8)
+    bg[0] = np.zeros((area, area, 3), dtype=np.uint8)
     bg[1] = np.zeros((plotsize, plotsize, 3), dtype=np.uint8)
     bg[2][0] = np.zeros((plotsize, plotsize, 1), dtype=np.double)
     bg[2][1] = np.zeros((plotsize, plotsize, 1), dtype=np.double)
     
     for atomv in atoms:
         try:        
-            distu = distugraph(atomv.phase, plotsize)
-            data = monoasim(plotsize, distu)
+            #distu = distugraph(atomv.phase, plotsize)
+            #data = monoasim(plotsize, distu)
+            data = datas[randint(0,5)]
             
             image = data[0]
             image2 = data[1] # Heatmap
@@ -53,11 +57,10 @@ for i in range(steps):
             bg[2][1][x:x2,y:y2] += data[2][1] # vertical field
             
         except: # If atom crush into wall, flip its velocity
-            if int(atomv.position[0]/8) < scaledsize or int(atomv.position[0]/8) > area - scaledsize:
+            if int(atomv.position[0]/8) < scaledsize or int(atomv.position[0]/8) > plotsize - scaledsize:
                 atomv.flip([True,False])
-            if int(atomv.position[1]/8) < scaledsize or int(atomv.position[1]/8) > area - scaledsize:
+            if int(atomv.position[1]/8) < scaledsize or int(atomv.position[1]/8) > plotsize - scaledsize:
                 atomv.flip([False,True])
-            
             
         
     for atomv in atoms:
@@ -66,9 +69,9 @@ for i in range(steps):
         try:
             atomv.step(1, [int(bg[2][0][x,y]*10), int(bg[2][1][x,y]*10)])
         except:
-            if int(atomv.position[0]/8) > area - scaledsize:
+            if int(atomv.position[0]/8) > plotsize - scaledsize:
                 atomv.flip([True,False])
-            if int(atomv.position[1]/8) > area - scaledsize:
+            if int(atomv.position[1]/8) > plotsize - scaledsize:
                 atomv.flip([False,True])
     Image.fromarray(bg[1]).save(f"../images/{i+1}.png")
     print(f"Processing ({i+1}/{steps})")
